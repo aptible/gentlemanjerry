45180edad7eb752d1692ff12a6914362ab6ca78d
Thomas Orozco <thomas@orozco.fr>
2016-04-20 20:08:27 +0200

    Inject a unique message ID in messages
    This will let a log reader compare their position in the stream to the
    position of the buffers they retrieved (e.g. to avoid outputting the
    same message twice).

--- templates/load-message.lua
+++ templates/load-message.lua
@@ -1,19 +1,21 @@
-local rawMessage = ARGV[1]
-
 -- Some constants to control the behavior. Eventually, we might want to control
 -- those via Redis keys.
 local bufferBucketInterval = 60 -- How long (in seconds) should a buffer bucket be?
 local bufferBucketCount = 20    -- How many buffer buckets to retain (memory permitting)
 
--- First, parse the message and figure out the app
-local message = cjson.decode(rawMessage)
+-- First, parse the incoming JSON message.
+local message = cjson.decode(ARGV[1])
+
+-- Prepare a Message ID, add it to the message, and serialize.
+message.redisMessageId = redis.call("INCR", "last-redisMessageId")
+local messageJson = cjson.encode(message)
 
 -- It's OK to index messages by app name, since app names are unique within an
 -- environment.
 local appName = message.app
 
 -- First, deliver the message to listeners.
-local publishRet = redis.call("PUBLISH", "stream-" .. appName, rawMessage)
+local publishRet = redis.call("PUBLISH", "stream-" .. appName, messageJson)
 
 -- Now, we need to add the key to a buffer. We actually want to use multiple buffers
 -- for each log stream, so that the Redis volatile-ttl policy will automatically
@@ -26,7 +28,7 @@ local bufferBucketName = "buffer-bucket-" .. appName .. "-" .. bufferBucketIndex
 -- Add the message to the bucket, and set (or refresh) a TTL on the bucket.
 -- This will ensure the message is eventually expired, and will also be used
 -- by Redis to inform OOM eviction decisions.
-redis.call("LPUSH", bufferBucketName, rawMessage)
+redis.call("LPUSH", bufferBucketName, messageJson)
 redis.call("EXPIRE", bufferBucketName, bufferBucketInterval * bufferBucketCount)
 
 -- Finally, store the buffer name in our buffer map. The buffer map is a sorted
