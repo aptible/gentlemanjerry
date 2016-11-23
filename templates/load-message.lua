1b0f1b094c9a235fa1c464a0a282a86460f8fcba
Thomas Orozco <thomas@orozco.fr>
2016-11-23 19:08:13 +0100

    Add log tail support for database logging
    This adds log tail support for database logs. This isn't really *needed*
    to release database logs (since Log Tails won't receive database logs
    until we decide to enable that feature on them), but it ensures
    Gentlemanjerry will understand these logs if it receives them.
    
    To maintain backwards compatibility with Dumptruck yet avoid
    embarrassing collisions between apps whose handle starts with `database`
    and databases themselves, we don't change the routing for app logs in
    Redis, but include the string `database:` for database logs (since `:`
    is not a valid character in a handle, we can't have collisions)
    
    (there is currently only one app whose handle starts with `database`, so
    the collision is really hypothetical here, but it's easy to get ahead of
    it, so..!)

--- templates/load-message.lua
+++ templates/load-message.lua
@@ -16,12 +16,28 @@ message.redisMessageId = tonumber(redis.call("INCR", "last-redisMessageId"))
 local messageJson = cjson.encode(message)
 
 -- We're going to need to know the app name to route messages, and the
--- timestamp for the message. Unfortunately we're not guaranteed that those
--- fields will be here. In particular, app may be missing if JoeCool didn't
--- send it or if Gentlemanjerry failed to parse it. When that happens, we'll
--- just route this message to a junk bucket. This is unlikely to be useful for
--- users, but may let us troubleshoot errors more easily.
-local messageAppName = message.app or "JUNK"
+-- timestamp for the message, so we'll start by extracting this.  Unfortunately
+-- we're not guaranteed that those fields will be here. In particular, app may
+-- be missing if JoeCool didn't send it or if Gentlemanjerry failed to parse
+-- it. When that happens, we'll just route this message to a junk bucket. This
+-- is unlikely to be useful for users, but may let us troubleshoot errors more
+-- easily.
+
+-- We used to only support app logs, but now we support databases. To avoid
+-- breaking things (mainly, to ease the transition in Dumptruck), we continue
+-- routing app keys to their old unprefixed name.
+
+local layer = message.layer or "JUNK"
+local messageRouting
+
+if layer == "database" then
+	messageRouting = layer .. ":" .. (message.database or "JUNK")
+else
+	messageRouting = message.app
+end
+
+messageRouting = messageRouting or "JUNK"
+
 local messageTimestamp = message.unix_timestamp or 0
 
 -- We'll index messages by app name, which is fine because app names are unique
@@ -29,7 +45,7 @@ local messageTimestamp = message.unix_timestamp or 0
 -- because users query logs on a per-app basis.
 
 -- Make sure we deliver the message to subscribers waiting for it.
-local publishRet = redis.call("PUBLISH", "stream-" .. messageAppName, messageJson)
+local publishRet = redis.call("PUBLISH", "stream-" .. messageRouting, messageJson)
 
 -- Now, we need to add the key to a buffer. We actually want to use multiple buffers
 -- for each log stream, so that the Redis volatile-ttl policy will automatically
@@ -37,7 +53,7 @@ local publishRet = redis.call("PUBLISH", "stream-" .. messageAppName, messageJso
 -- buffer logs into a new bucket every minute, which means we'll have 10 active
 -- buckets per log stream, since our TTL on log buffers is 10 minutes.
 local bufferBucketIndex = math.floor(messageTimestamp / pBufferBucketInterval) * pBufferBucketInterval
-local bufferBucketName = "buffer-bucket-" .. messageAppName .. "-" .. bufferBucketIndex
+local bufferBucketName = "buffer-bucket-" .. messageRouting .. "-" .. bufferBucketIndex
 
 -- New messages are added at the right end of the bucket, which means that
 -- messages will be sorted from first received to last received (note: the
@@ -53,7 +69,7 @@ redis.call("EXPIREAT", bufferBucketName, messageTimestamp + pBufferLifetime)
 -- Finally, store the bucket name in a buffer map. The buffer map is a sorted
 -- set where we sort bucket names by their timestamp. Thus, when we query the
 -- set via ZRANGE ... 0 -1, we'll get the buckets from oldest to newest.
-local bufferMapName = "buffer-map-" .. messageAppName
+local bufferMapName = "buffer-map-" .. messageRouting
 redis.call("ZADD", bufferMapName, bufferBucketIndex, bufferBucketName)
 
 -- Truncate the buffer map to only retain the address of the buffers that may
